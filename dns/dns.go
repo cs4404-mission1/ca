f1e69af7cfb6bc1366fcada1ff0ba4738c4ca9f0
Nate Sales <nate@natesales.net>
2022-11-01 14:36:33 -0400

    feat: add dns poisoning step

---
+++ dns/dns.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"math/rand"
+	"net"
+
+	"github.com/miekg/dns"
+)
+
+var (
+	local  = flag.String("l", "10.64.10.3", "local IP address")
+	remote = flag.String("r", "10.64.10.2", "DNS server")
+)
+
+func main() {
+	flag.Parse()
+
+	// Create DNS message
+	m := new(dns.Msg)
+	m.Id = uint16(rand.Intn(65535))
+	m.RecursionDesired = true
+	m.Question = []dns.Question{{
+		Name:   "api.internal.",
+		Qtype:  dns.TypeA,
+		Qclass: dns.ClassINET,
+	}}
+
+	conn, err := net.DialUDP(
+		"udp",
+		&net.UDPAddr{IP: net.ParseIP(*local), Port: 50000},
+		&net.UDPAddr{IP: net.ParseIP(*remote), Port: 53},
+	)
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := dns.Client{Net: "udp"}
+	r, _, err := client.ExchangeWithConn(m, &dns.Conn{Conn: conn})
+	if err != nil {
+		log.Fatalf("DNS exchange: %s", err)
+	}
+
+	log.Printf("Status: %d, ID: %d", r.MsgHdr.Rcode, r.Id)
+	for _, a := range r.Answer {
+		if t, ok := a.(*dns.A); ok {
+			log.Println(t.A.String())
+		}
+	}
+}
---
+++ dns/go.mod
@@ -0,0 +1,17 @@
+module github.com/natesales/cs4404-mission1/dns
+
+go 1.19
+
+require (
+	github.com/google/gopacket v1.1.19
+	github.com/miekg/dns v1.1.50
+	github.com/vishvananda/netlink v1.1.0
+)
+
+require (
+	github.com/vishvananda/netns v0.0.0-20221028002847-e414ad8e04b4 // indirect
+	golang.org/x/mod v0.6.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
+)
